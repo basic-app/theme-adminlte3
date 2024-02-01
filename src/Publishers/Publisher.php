1a4387ed179f9e53732a44fa5833b3f7cc7399c0
basic-app <dev@basic-app.com>
2024-02-01 11:09:00 +0300

    n/a

--- src/Publishers/Publisher.php
+++ src/Publishers/Publisher.php
@@ -1,33 +1,20 @@
 <?php
-
+/**
+ * @author Basic App Dev Team
+ * @license MIT
+ */
 namespace Themes\AdminLTE3\Publishers;
 
 use CodeIgniter\Publisher\Publisher as BasePublisher;
-use Config\Publisher as PublisherConfig;
+use Config\Publisher as Config;
 
 class Publisher extends BasePublisher
 {
-    /**
-     * Tell Publisher where to get the files.
-     * Since we will use Composer to download
-     * them we point to the "vendor" directory.
-     *
-     * @var string
-     */
+
     protected $source = VENDORPATH . 'almasaeed2010' . DIRECTORY_SEPARATOR . 'adminlte';
 
-    /**
-     * FCPATH is always the default destination,
-     * but we may want them to go in a sub-folder
-     * to keep things organized.
-     *
-     * @var string
-     */
     protected $destination = FCPATH . 'themes' . DIRECTORY_SEPARATOR . 'adminlte3';
 
-    /**
-     * Makes destination directory.
-     */
     public function __construct(?string $source = null, ?string $destination = null)
     {
         if (!is_dir($this->destination))
@@ -35,26 +22,29 @@ class Publisher extends BasePublisher
             mkdir($this->destination, 0755, true);
         }
 
+        $config = config(Config::class);
+
+        $originalRestrictions = $config->restrictions[FCPATH];
+
+        $config->restrictions[FCPATH] = str_replace('#\.', '#LICENSE|\.', $config->restrictions[FCPATH]);
+        $config->restrictions[FCPATH] = str_replace(')$#', '|txt)$#', $config->restrictions[FCPATH]);
+
         parent::__construct($source, $destination);
+
+        $config->restrictions[FCPATH] = $originalRestrictions;
     }
 
-    /**
-     * Use the "publish" method to indicate that this
-     * class is ready to be discovered and automated.
-     */
     public function publish(): bool
     {
         return $this->wipe()
             ->addPath('plugins')
             ->addPath('dist')
-            ->removePattern('*.txt')
             ->removePattern('*.js.flow')
             ->removePattern('*.less')
             ->removePattern('*.ts')
             ->removePattern('*.editorconfig')
             ->removePattern('*.md')
             ->removePattern('*.npmignore')
-            ->removePattern('LICENSE')
             ->merge(true);
     }
 
